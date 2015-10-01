795ae121ab7e921743f94d0a4884bd904d5d0652
Chris Seto <chriskseto@gmail.com>
2015-10-01 14:30:32 -0400

    Implement namespace

---
+++ wdim/client/namespace.py
@@ -0,0 +1,29 @@
+from asyncio_mongo import filter as qf
+
+from wdim.util import pack
+from wdim.client import fields
+from wdim.client.storable import Storable
+from wdim.client.collection import Collection
+
+
+class Namespace(Storable):
+
+    _id = fields.ObjectIdField()
+    namespace = fields.StringField(unique=True)
+
+    @classmethod
+    async def get_by_namespace(cls, name):
+        return await cls.find_one({'namespace': name})
+
+    async def get_collection(self, collection_name):
+        assert self._id, 'Namespace must be saved before use'
+
+        return await Collection.find_one({
+            'namespace': self._id,
+            'collection': collection_name
+        })
+
+    async def create_collection(self, collection_name):
+        assert self._id, 'Namespace must be saved before use'
+
+        return await Collection.create(collection=collection_name, namespace=self)
