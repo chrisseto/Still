0734b781b717e79135d7c020892fc97c915d8900
Chris Seto <chriskseto@gmail.com>
2015-10-05 15:53:26 -0400

    Use new query syntax

--- wdim/client/namespace.py
+++ wdim/client/namespace.py
@@ -10,15 +10,15 @@ class Namespace(Storable):
 
     @classmethod
     async def get_by_namespace(cls, name):
-        return await cls.find_one({'namespace': name})
+        return await cls.find_one(Namespace.namespace == name)
 
     async def get_collection(self, collection_name):
         assert self._id, 'Namespace must be saved before use'
 
-        return await Collection.find_one({
-            'namespace': self._id,
-            'collection': collection_name
-        })
+        return await Collection.find_one(
+            (Collection.namespace == self._id) &
+            (Collection.collection == collection_name)
+        )
 
     async def create_collection(self, collection_name):
         assert self._id, 'Namespace must be saved before use'
