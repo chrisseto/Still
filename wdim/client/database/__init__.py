861275a48d1fea66259ccc27e126acdcfb76d952
Chris Seto <chriskseto@gmail.com>
2015-10-05 15:55:26 -0400

    Update __all__

--- wdim/client/database/__init__.py
+++ wdim/client/database/__init__.py
@@ -1,5 +1,10 @@
 from wdim.client.database.mongo import MongoLayer
+from wdim.client.database.compound import CompoundWriteLayer
 from wdim.client.database.elasticsearch import ElasticSearchLayer
 
 
-__all__ = ('MongoLayer', 'ElasticSearchLayer')
+__all__ = (
+    'MongoLayer',
+    'CompoundWriteLayer',
+    'ElasticSearchLayer'
+)
