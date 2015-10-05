39086b074dbac8d6d743ede09ce3556e4861e5a4
Chris Seto <chriskseto@gmail.com>
2015-10-05 09:08:10 -0400

    Reimplement Blob, switch to sha256

--- wdim/client/blob.py
+++ wdim/client/blob.py
@@ -1,31 +1,27 @@
 import json
 import hashlib
 
+from wdim import exceptions
+from wdim.client import fields
 from wdim.client.storable import Storable
 
 
 class Blob(Storable):
 
-    HASH_METHOD = 'sha1'
+    HASH_METHOD = 'sha256'
+
+    _id = fields.StringField(unique=True)
+    data = fields.DictField()
 
     @classmethod
-    def _create(cls, data):
-        sha = hashlib(cls.HASH_METHOD, json.dumps(data))
-        return cls(sha, data)
-
-    @classmethod
-    def _from_document(cls, document):
-        return cls(document['data'])
+    async def create(cls, data):
+        sha = hashlib.new(cls.HASH_METHOD, json.dumps(data).encode('utf-8')).hexdigest()
+        try:
+            # Classmethod supers need arguments for some reason
+            return await super(Blob, cls).create(_id=sha, data=data)
+        except exceptions.UniqueViolation:
+            return await cls.load(sha)
 
     @property
     def hash(self):
         return self._id
-
-    def __init__(self, data):
-        self.data = data
-
-    def to_document(self):
-        return {
-            '_id': self.hash,
-            'data': self.data
-        }
