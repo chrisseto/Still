7f8cc5da6d991378b9b38c758cdf70a0293174c3
Chris Seto <chriskseto@gmail.com>
2015-10-05 15:54:10 -0400

    Make database layers composable

--- wdim/client/database/base.py
+++ wdim/client/database/base.py
@@ -3,24 +3,36 @@ import abc
 
 class DatabaseLayer(metaclass=abc.ABCMeta):
 
-    @classmethod
     @abc.abstractmethod
-    def translate_query(self, query):
-        pass
+    async def ensure_index(self, name, keys):
+        raise NotImplemented
 
-    @classmethod
-    @abc.abstractmethod
-    def translate_sorting(self, query):
-        pass
-
+    # Create
     @abc.abstractmethod
     async def insert(self, inst):
         raise NotImplemented
 
+    # Read
     @abc.abstractmethod
     async def load(self, pk):
         raise NotImplemented
 
     @abc.abstractmethod
-    async def ensure_index(self, name, keys):
+    async def find(self, query):
         raise NotImplemented
+
+    @abc.abstractmethod
+    async def find_one(self, query):
+        raise NotImplemented
+
+    # Update
+
+    # Delete
+    @abc.abstractmethod
+    async def drop(self, pk):
+        raise NotImplemented
+
+    def __rshift__(self, other):
+        # TODO resolve circular import
+        from wdim.client.database import CompoundWriteLayer
+        return CompoundWriteLayer(self, other)
