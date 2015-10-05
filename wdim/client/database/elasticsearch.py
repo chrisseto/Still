45265f65afcfbad402714690b15c22001760f369
Chris Seto <chriskseto@gmail.com>
2015-10-05 15:54:55 -0400

    Implement elasticsearch database layer

--- wdim/client/database/elasticsearch.py
+++ wdim/client/database/elasticsearch.py
@@ -1,58 +1,151 @@
 import abc
+import json
+import operator
+import datetime
 from functools import reduce
+
+import furl
+
+import aiohttp
 
 import elasticsearch_dsl
 
 from wdim import exceptions
 from wdim.client import sort
 from wdim.client import query
+from wdim.client import fields
 from wdim.client.database.base import DatabaseLayer
+from wdim.client.database.translation import Translator
+
+
+class ElasticSearchTranslator(Translator):
+
+    @classmethod
+    def translate_query(cls, q):
+        if isinstance(q, query.Query):
+            q._value = cls.translate_value(q.value)
+
+        try:
+            return {
+                query.And: lambda: reduce(operator.and_, (cls.translate_query(x) for x in q.querys)),
+                query.Equals: lambda: elasticsearch_dsl.Q('match', **{q.name: q.value})
+            }[q.__class__]()
+        except KeyError:
+            raise exceptions.OperationNotSupported(query)
+
+    @classmethod
+    def translate_sorting(cls, sorting):
+        try:
+            return {
+                sort.Ascending: sorting.field._name,
+                sort.Descending: '-' + sorting.field._name
+            }[sorting.__class__]
+        except KeyError:
+            raise exceptions.OperationNotSupported(query)
+
+    @classmethod
+    def translate_field(cls, field, value):
+        try:
+            # TODO should probably just match on type
+            return {
+                fields.DatetimeField: lambda: value.isoformat(),
+                fields.ObjectIdField: lambda: str(value),
+                fields.ForeignField: lambda: str(value)
+            }[field.__class__]()
+        except KeyError:
+            return field.to_document(value)
+
+    @classmethod
+    def translate_value(cls, value):
+        try:
+            # TODO should probably just match on type
+            return {
+                fields.bson.ObjectId: lambda: str(value),
+                datetime.datetime: lambda: value.isoformat(),
+            }[value.__class__]()
+        except KeyError:
+            return value
 
 
 class ElasticSearchLayer(DatabaseLayer):
 
     @classmethod
-    def connect(cls, address, index_name):
-        pass
+    async def connect(cls, host='localhost', index_name='wdim20150921', port=9200):
+        return cls(host, port, index_name)
 
-    @classmethod
-    def translate_query(cls, q: query.BaseQuery):
-        assert isinstance(q, query.BaseQuery), 'q must be an instance of BaseQuery'
-
-        try:
-            return {
-                query.Equals: lambda: {q.name: q.value},
-                query.And: lambda: reduce(lambda x, y: {**x, **y}, (cls.translate_query(qu) for qu in q.querys), {})
-            }[q.__class__]()
-        except KeyError:
-            raise exceptions.UnsupportedOperation(q)
-
-    @classmethod
-    def translate_sorting(self, sorting):
-        try:
-            return qf.sort({
-                sort.Ascending: qf.DESCENDING,
-                sort.Descending: qf.ASCENDING,
-            }[sorting.__class__](sorting.fields._name))
-        except KeyError:
-            raise exceptions.UnsupportedOperation(sorting)
-
-    def __init__(self, address, index_name):
-        self.furl = furl.furl(address)
-        self.furl.path.append(index_name)
+    def __init__(self, address, port, index_name):
+        self.furl = furl.furl()
+        self.furl.port = port
+        self.furl.host = address
+        self.furl.scheme = 'http'
+        self.furl.path.add(index_name)
 
     async def load(self, cls, _id):
+        return await self.find_one(cls, cls._id == _id)
+
+    async def find_one(self, cls, query):
+        search = elasticsearch_dsl.Search().query(ElasticSearchTranslator.translate_query(query))[:1]
+        response = await self._send_request('GET', cls._collection_name, query=search)
+
+        if len(response.hits) == 0:
+            raise exceptions.NotFound(query)
+
+        return response.hits[0].to_dict()
+
+    async def find(self, cls, query=None, limit=None, skip=0, sort=None):
+        search = elasticsearch_dsl.Search()
+
+        if query:
+            search.query(ElasticSearchTranslator.translate_query(query))
+        if limit or skip:
+            search = search[skip:skip + limit]
+        if sort:
+            search.sort(ElasticSearchTranslator.translate_sorting(sort))
+
+        response = await self._send_request('GET', cls._collection_name, query=search)
+
+        if len(response.hits) == 0:
+            raise exceptions.NotFound()
+
+        return (result.to_dict() for result in response.hits)
+
+    async def ensure_index(self, cls, indices):
         pass
 
-    async def insert(self, cls, inst):
-        resp = await aiohttp.request(
-            'PUT',
-            self.host + '',
-            data=json.dumps(inst.to_document())
-        )
+    async def drop(self, cls):
+        # TODO validate return
+        resp = await self._send_request('DELETE', cls._collection_name)
 
     async def insert(self, inst):
+        copied = self.furl.copy()
+        copied.path.segments.append(inst.__class__._collection_name)
+        if inst._id:
+            copied.path.segments.append(str(inst._id))
+
+        resp = await aiohttp.request(
+            'PUT',
+            copied.url,
+            data=json.dumps(inst.to_document(ElasticSearchTranslator))
+        )
+        assert resp.status in (200, 201)
+
         try:
-            return await self.connection[inst._collection_name].insert(inst.to_document(), safe=True)
-        except _pymongo.errors.DuplicateKeyError:
-            raise exceptions.UniqueViolation
+            return (await resp.json())['_id']
+        finally:
+            resp.close()
+
+    async def _send_request(self, method, _type, query=None, _id=None):
+        copied = self.furl.copy()
+        copied.path.segments.append(_type)
+        if _id:
+            copied.path.segments.append(_id)
+        else:
+            copied.path.segments.append('_search')
+
+        resp = await aiohttp.request(
+            method,
+            copied.url,
+            data=json.dumps(query.to_dict()) if query else None
+        )
+
+        return elasticsearch_dsl.result.Response(await resp.json())
