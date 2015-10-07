baa1791258bfb4d98ba05dfdd732dfbbcc1427c0
Chris Seto <chriskseto@gmail.com>
2015-10-07 13:48:10 -0400

    Remove old code

--- wdim/client/schema.py
+++ wdim/client/schema.py
@@ -24,10 +24,7 @@ class BaseSchema(metaclass=abc.ABCMeta):
     def name(self, schema):
         raise NotImplemented
 
-    def __init__(self, _id, schema, version=None):
-        self._id = _id
-        if version is not None:
-            self._id += '::' + version
+    def __init__(self, schema):
         self.schema = schema
 
     def validate(self, data):
